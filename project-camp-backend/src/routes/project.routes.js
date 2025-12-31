d37780fa4efe938715494452971513ba14a9e2af
suraj kengar <surajkengar1212@gmail.com>
2025-12-31 22:53:35 +0530

    project.routes.js file is added

---
+++ project-camp-backend/src/routes/project.routes.js
@@ -0,0 +1,59 @@
+import { Router } from "express";
+import {
+  addMemberToProject,
+  createProject,
+  deleteMember,
+  deleteProject,
+  getProjectById,
+  getProjectMembers,
+  getProjects,
+  updateMemberRole,
+  updateProject,
+} from "../controllers/project.controllers.js";
+import {
+  validateProjectPermission,
+  verifyJWT,
+} from "../middlewares/auth.middleware.js";
+import { validate } from "../middlewares/validator.middleware.js";
+import { AvailableUserRoles, UserRolesEnum } from "../utils/constants.js";
+import {
+  addMemberToProjectValidator,
+  createProjectValidator,
+} from "../validators/index.js";
+
+const router = Router();
+
+router.use(verifyJWT);
+
+router
+  .route("/")
+  .get(getProjects)
+  .post(createProjectValidator(), validate, createProject);
+
+router
+  .route("/:projectId")
+  .get(validateProjectPermission(AvailableUserRoles), getProjectById)
+  .put(
+    validateProjectPermission([UserRolesEnum.ADMIN]),
+    createProjectValidator(),
+    validate,
+    updateProject,
+  )
+  .delete(validateProjectPermission([UserRolesEnum.ADMIN]), deleteProject);
+
+router
+  .route("/:projectId/members")
+  .get(getProjectMembers)
+  .post(
+    validateProjectPermission([UserRolesEnum.ADMIN]),
+    addMemberToProjectValidator(),
+    validate,
+    addMemberToProject,
+  );
+
+router
+  .route("/:projectId/members/:userId")
+  .put(validateProjectPermission([UserRolesEnum.ADMIN]), updateMemberRole)
+  .delete(validateProjectPermission([UserRolesEnum.ADMIN]), deleteMember);
+
+export default router;
